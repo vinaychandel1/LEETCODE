a24d7640d4786c78889390d8e326e1a19066b575
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-04-22 22:16:49 +0530

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       set<int> s(nums.begin(), nums.end());
+if(s.size() != nums.size()){
+    return true;
+    
+}
+        return false;
+    }
+};
