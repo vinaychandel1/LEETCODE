d0a7c079e973cc45aa35b8d93bffdc5f4338a038
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-04-23 13:10:32 +0530

    Update 23. Bitwise AND of Numbers Range.c

--- 30-Day LeetCoding Challenge/23. Bitwise AND of Numbers Range.c
+++ 30-Day LeetCoding Challenge/23. Bitwise AND of Numbers Range.c
@@ -4,3 +4,19 @@ int rangeBitwiseAnd(int m, int n){
  return (n > m) ? (rangeBitwiseAnd(m/2, n/2) << 1) : m;
 }
 
+/*OR
+
+
+int rangeBitwiseAnd(int m, int n){
+int c=0;
+	while(m!=n){
+		m>>=1;
+		n>>=1;
+		++c;
+	}
+	return n<<c;
+}
+
+
+*/
+
