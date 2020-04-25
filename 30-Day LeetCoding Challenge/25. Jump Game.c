5dd1b01d567ee32c1c612695941cfbed28b0562f
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-04-25 13:28:36 +0530

    Update 25. Jump Game.c

--- 30-Day LeetCoding Challenge/25. Jump Game.c
+++ 30-Day LeetCoding Challenge/25. Jump Game.c
@@ -11,7 +11,19 @@ bool canJump(int* nums, int numsSize){
         if(jump>=0)
             return true;
         return false;
-
-
 }
+
+/* c++ solution 
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int distance = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            distance = max(distance, i + nums[i]);
+            if(distance == i) return false;
+        }
+        return true;
+    }
+};
+*/
 
