ae0687df7d032139f38322e1f4bc5bdcb1a6cea2
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-05-01 22:00:18 +0530

    Update 175. Combine Two Tables.sql

--- DBMS/175. Combine Two Tables.sql
+++ DBMS/175. Combine Two Tables.sql
@@ -1,3 +1,10 @@
 # Write your MySQL query statement below
 Select Person.FirstName, Person.LastName, Address.City, Address.State
 From Person left join Address on Person.PersonId = Address.PersonId
+
+
+/*OR
+select FirstName, LastName, City, State
+from Person left join Address
+on Person.PersonId = Address.PersonId
+;*/
