a00836d8ecdd07d2269d9d8939824975003f21ba
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2020-06-09 23:57:26 +0530

    Update 231. Power of Two.c

--- 231. Power of Two.c
+++ 231. Power of Two.c
@@ -2,3 +2,18 @@
 bool isPowerOfTwo(int x) {
 	return !(x<=0 || (x & (x - 1)));
 }
+/*or
+isPowerOfTwo(int n) {
+        if(n < 1)
+            return false;
+        if(n == 1)
+            return true;
+        if(n %2 ==1)
+            return false;
+        return (isPowerOfTwo(n/2));
+    }
+*/
+/*OR
+n = n&(n-1);
+return n==0;
+*/
