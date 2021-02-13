617b2f3b81fa73e6e4a8dcdd373adf0680697fd3
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-02-13 22:12:46 +0530

    Update 43. Multiply Strings.py

--- 43. Multiply Strings.py
+++ 43. Multiply Strings.py
@@ -1,6 +1,4 @@
-class Solution {
-public:
-    string multiply(string num1, string num2) {
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
         
-    }
-};
