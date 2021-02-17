1ca657f3c133f8ad77a8ea4c633efd3271ec2e26
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-02-17 16:29:05 +0530

    Create 941. Valid Mountain Array.cpp

---
+++ 941. Valid Mountain Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int i=0;
+        if(arr.size()<3){
+            return false;
+        }
+      while(i+1<arr.size() && arr[i]<arr[i+1]) i++;
+        if(i==0 || i==arr.size()-1) return false;
+      while(i+1<arr.size() && arr[i]>arr[i+1]) i++;
+        return i==arr.size()-1;
+    }
+};
